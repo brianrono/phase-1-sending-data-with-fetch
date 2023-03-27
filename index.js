d1b03bec8997738bb6ea2078a9cb326aba17fc56
brianrono <brianrono16@gmail.com>
2023-03-27 15:41:09 +0300

    extended

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Add your code here
+fetch("http://localhost:3000/dogs", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify({
+    dogName: "Byron",
+    dogBreed: "Poodle",
+  }),
+});
+
+
+// This code brings about the same results as the above
+
+
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   };
+  
+//   const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//     },
+//     body: JSON.stringify(formData),
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configurationObject);
+
+
+// Same case as above
+
+
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//     },
+//     body: JSON.stringify({
+//       dogName: "Byron",
+//       dogBreed: "Poodle",
+//     }),
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configurationObject);
