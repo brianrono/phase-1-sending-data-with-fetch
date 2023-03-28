6520ff5e0ea4362364e07d6fc01227fda84bfa03
brianrono <brianrono16@gmail.com>
2023-03-28 11:22:30 +0300

    Complete

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
-fetch("http://localhost:3000/dogs", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  },
-  body: JSON.stringify({
-    dogName: "Byron",
-    dogBreed: "Poodle",
-  }),
-});
+function submitData(userName,userEmail) {
+  const FormData = {
+      name: userName,
+      email: userEmail
+  }
+  const configurationObj = {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+      },
+      body: JSON.stringify(FormData)
+  }
+ let p = document.createElement("p")
+ return fetch("http://localhost:3000/users",configurationObj)
+  .then(res => res.json())
+  .then(data => {
+      let p = document.createElement("p")
+      p.textContent = data.id
+      document.querySelector("body").appendChild(p)
+ })
+ .catch(err => {
+  let p = document.createElement("p")
+  p.textContent = err.message;
+  document.querySelector("body").appendChild(p)
+ })
+}
+
+
+
+// fetch("http://localhost:3000/dogs", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//   },
+//   body: JSON.stringify({
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   }),
+// });
 
 
 // This code brings about the same results as the above
